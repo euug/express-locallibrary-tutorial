e9040209da687d5806b1838879fe7e1a00345588
Eugene Joy <eugene.joy@live.ca>
2020-11-25 23:27:07 -0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = "mongodb+srv://ejoy:mango69@cluster0.6zcqp.mongodb.net/local_library?retryWrites=true&w=majority";
+var dev_db_url = 'mongodb+srv://ejoy:mango69@cluster0.6zcqp.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
